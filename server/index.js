7c2fccb43a833ae3aff6068d1a8d1ca9af537e38
Leo Nard <yz2526@web.de>
2025-08-12 13:01:33 +0200

    fixed npm start script

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ app.get("/api", (req, res) => {
 });
 
 // All other GET requests not handled before will return our React app
-app.get("'/{*any}'", (req, res) => {
+// app.get("*", (req, res) => {
+app.get("/{*any}", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
