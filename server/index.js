08bd7fe15c38c610338889195cfdca7eeba08449
Leo Nard <yz2526@web.de>
2025-08-12 12:52:39 +0200

    fixed catch-all route

--- server/index.js
+++ server/index.js
@@ -10,11 +10,11 @@ app.get("/api", (req, res) => {
 	res.json({ message: "Hello from server!" });
 });
 
-app.listen(PORT, () => {
-	console.log("Server is listening on " + PORT);
+// All other GET requests not handled before will return our React app
+app.get("'/{*any}'", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
-// All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+app.listen(PORT, () => {
+	console.log("Server is listening on " + PORT);
 });
